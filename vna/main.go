48ff6eb38e0a8614467181ae3ef03bb8af169f22
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-10-05 16:43:47 +0100

    Add empty go main

---
+++ vna/main.go
@@ -0,0 +1,17 @@
+//
+// main.go
+// Copyright (C) 2019 Tim Hughes
+//
+// Distributed under terms of the MIT license.
+//
+package main
+
+/*
+#cgo CFLAGS: -g -Wall
+#cgo LDFLAGS: -L. -lPocketVnaApi_x64
+#include "pocketvna.h"
+*/
+import "C"
+
+func main() {
+}
